2dd4582d5a7558977adc6a1a75b932cd3190f3e6
Alexis López <arlopez@grupozurita.com.ec>
2025-06-11 10:07:05 -0500

    componente principal pantalla de entrada

---
+++ src/app/views/main/main.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss'],
+   animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(10px)' }),
+        animate('800ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+      ])
+    ])
+  ]
+})
+export class MainComponent implements OnInit {
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+  }
+
+  goToMain() {
+    this.router.navigate(['/main']);
+  }
+
+
+
+}
